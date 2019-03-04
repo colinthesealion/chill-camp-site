257a7e38d193234bf3b7eac7674f723fba4bd1fc
Colin Barringer <walaruigio@gmail.com>
2019-03-03 22:43:55 -0500

    added layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from '@reach/router';
 import injectSheet from 'react-jss';
 
 import styleSheet from './common.css';
+import layout from './Layout.png';
 
 interface Props extends RouteComponentProps {
   classes: {
@@ -13,7 +14,11 @@ const About: React.FunctionComponent<Props> = ({ classes }) => {
   return (
     <div className={classes.container}>
       <div className={classes.content}>
-        <p>Coming soon.</p>
+        <p>
+          This layout assumes we are in the woods, which is our preference, but
+          we can adapt to different terrain.
+        </p>
+        <img src={layout} alt="layout" />
       </div>
     </div>
   );
